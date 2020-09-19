dc278bc255cec5862ef68aeb022b72f78c211d35
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-09-19 14:18:18 -0400

    Random sounds tested/working

---
+++ Assets/Scripts/BounceSoundTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceSoundTest : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    void OnCollisionEnter()  //Plays Sound Whenever collision detected
+    {
+        SoundManager.soundManager.PlayBounceSound();
+    }
+
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager soundManager;
+    private AudioSource audioSource;
+    private AudioClip[] bounceSounds;
+    private AudioClip[] smashSounds;
+
+    void Start()
+    {
+        soundManager = this;
+        audioSource = GetComponent<AudioSource>();
+        bounceSounds = Resources.LoadAll<AudioClip>("bounce");
+        smashSounds = Resources.LoadAll<AudioClip>("smash");
+    }
+
+    public void PlayBounceSound()
+    {
+        int randomIndex = Random.Range(0, bounceSounds.Length);
+        audioSource.PlayOneShot(bounceSounds[randomIndex]);
+    }
+
+    public void PlaySmashSound()
+    {
+        int randomIndex = Random.Range(0, smashSounds.Length);
+        audioSource.PlayOneShot(smashSounds[randomIndex]);
+    }
+}
