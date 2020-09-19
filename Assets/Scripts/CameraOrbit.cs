b2c4238a41eeab36264cf456625f55da85f64322
Josh <wiizrdz@outlook.com>
2020-09-19 11:35:14 -0400

    Created orbit camera scene

---
+++ Assets/Scripts/CameraOrbit.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    private Transform xFormCamera;
+    private Transform xFormParent;
+
+    private Vector3 localRotation;
+    private float cameraDistance = 10f;
+
+    public float mouseSens = 4f;
+    public float scrollSens = 2f;
+    public float orbitDampening = 10f;
+    public float scrollDampening = 6f;
+
+    public bool cameraDisabled = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.xFormCamera = this.transform;
+        this.xFormParent = this.transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            cameraDisabled = !cameraDisabled;
+        }
+
+        if (!cameraDisabled)
+        {
+            // Rotation
+            if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+            {
+                localRotation.x += Input.GetAxis("Mouse X") * mouseSens;
+                localRotation.y -= Input.GetAxis("Mouse Y") * mouseSens;
+
+                // Prevent camera flip
+                localRotation.y = Mathf.Clamp(localRotation.y, 0f, 90f);
+            }
+
+            // Zoom
+            if (Input.GetAxis("Mouse ScrollWheel") != 0)
+            {
+                float scrollAmount = Input.GetAxis("Mouse ScrollWheel") * scrollSens;
+                scrollAmount *= this.cameraDistance * 0.3f;
+
+                this.cameraDistance += scrollAmount * -1f;
+
+                this.cameraDistance = Mathf.Clamp(this.cameraDistance, 1.5f, 100f);
+            }
+        }
+    }
+
+    // Called after Update()
+    private void LateUpdate()
+    {
+        Quaternion qt = Quaternion.Euler(localRotation.y, localRotation.x, 0);
+
+        this.xFormParent.rotation = Quaternion.Lerp(this.xFormParent.rotation, qt, Time.deltaTime * orbitDampening);
+
+        if (this.xFormCamera.localPosition.z != this.cameraDistance * -1f)
+        {
+            this.xFormCamera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this.xFormCamera.localPosition.z, this.cameraDistance * -1f, Time.deltaTime * scrollDampening));
+        }
+    }
+}
