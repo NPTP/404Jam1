f2f9e8a95dce8d86c6db484f2dbfd587bdb08e10
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-09-19 13:21:57 -0400

    In-game UI scene & prefabs

---
+++ Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreUI : MonoBehaviour
+{
+    public int numDestroyed;
+    public int destroyGoal = 10;
+    public Text scoreNumber;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            numDestroyed++;
+        }
+
+        scoreNumber.text = (numDestroyed * 100).ToString("0");
+        if (numDestroyed == destroyGoal)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
