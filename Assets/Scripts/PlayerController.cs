f87244e2ed06040f3f23c981c124f8311bab15cb
Denny Wiseman <wisemandenny@gmail.com>
2020-09-19 13:08:09 -0400

    added player controllable ball w/ bounce and move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 1;
+    public float rotationSpeed = 0;
+
+    private float movementX;
+    private float movementY;
+    private bool jump;
+
+    
+    private Rigidbody rb;
+
+    public float jumpHeight = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = false;
+    }
+
+    void Update()
+    {
+        if (rb.velocity.y == 0)
+        {
+            jump = false;
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float jumpMovement = 0f;
+        if (Input.GetKeyDown(KeyCode.Space) && !jump)
+        {
+            jumpMovement = jumpHeight;
+            jump = true;
+        }
+        Vector3 movement = new Vector3(moveHorizontal, jumpMovement, moveVertical);
+        rb.AddForce(movement * movementSpeed);
+    }
+
+}
