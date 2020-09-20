a9757f271c5fc9af3207dbdbee7721dbb43548b6
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-09-20 17:41:03 -0400

    Names on title screen, highlight on start button, sounds for button

---
+++ Assets/HoverButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverButton : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public void HoverSound()
+    {
+        audioSource.Play();
+    }
+}
--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -3,11 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour
 {
+    public AudioSource audioSource;
+
     public void StartGame()
     {
+        audioSource.Play();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // Unused
     public void QuitGame()
     {
         Application.Quit();
