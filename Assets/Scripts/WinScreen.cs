146aca4ae0179a172e3faf387ac1c323a82a4edc
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-09-22 13:01:55 -0400

    Add win screen, fix build order

---
+++ Assets/Scripts/WinScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    public AudioSource audioSrc;
+
+    public void PlayAgain()
+    {
+        audioSrc.Play();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    // Unused
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
